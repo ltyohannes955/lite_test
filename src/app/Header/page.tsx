2adec316d742bbfed3ae96563ebdbda1183bdd09
ltyohannes955 <ltyohannes95@gmail.com>
2024-11-14 13:32:49 +0300

    test

--- src/app/Header/page.tsx
+++ src/app/Header/page.tsx
@@ -1,28 +1,35 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
-const   Header = () => {
-    return(
 
-        <>
-        <div className="h-[80px] w-full flex flex-col items-center justify-center bg-gradient-to-br from-[#5C1B96] via-[#5D4792] via-[#5E7E8C] via-[#7B549A] via-[#9E21AA] via-[#A5139C] to-[#C825B8]">
-        
-            <div className="w-full flex justify-between items-center px-8 absolute top-0 h-16">
-                    <p className="text-white font-[Jaro] text-lg">
-                        LIYT
-                    </p>
-                    <div className="flex space-x-6 text-white">
-                        <Link href={"/Landing"}> <p>Home</p></Link>
-                        <Link href={"/About"}> <p>About</p></Link>
-                        <Link href={"/Dashboard"}> <p>Dashboard</p></Link>
-                    </div>
-                    <button
-                        className="text-white border border-white px-4 py-2 rounded-lg hover:bg-white hover:text-purple-600 transition" onClick={() => window.location.href = "/signup"}
-                    >
-                        Sign Up
-                    </button>
-                </div>
+const Header = () => {
+  return (
+    <>
+      <div className="h-[80px] w-full flex flex-col items-center justify-center bg-gradient-to-br from-[#5C1B96] via-[#5D4792] via-[#5E7E8C] via-[#7B549A] via-[#9E21AA] via-[#A5139C] to-[#C825B8]">
+        <div className="w-full flex justify-between items-center px-8 absolute top-0 h-16">
+          <p className="text-white font-[Jaro] text-lg">LIYT</p>
+          <div className="flex space-x-6 text-white">
+            <Link href="/Landing">
+              <p>Home</p>
+            </Link>
+            <Link href="/About">
+              <p>About</p>
+            </Link>
+            <Link href="/Dashboard">
+              <p>Dashboard</p>
+            </Link>
+          </div>
+          <button
+            className="text-white border border-white px-4 py-2 rounded-lg hover:bg-white hover:text-purple-600 transition"
+            onClick={() => (window.location.href = "/signup")}
+          >
+            Sign Up
+          </button>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
-export default Header
+
+export default Header;
