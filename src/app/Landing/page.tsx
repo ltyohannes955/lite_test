0eeab2035cafa599c19e11bb9ae0f53fe6d68aad
ltyohannes955 <ltyohannes95@gmail.com>
2024-11-18 09:23:34 +0300

    some edits

--- src/app/Landing/page.tsx
+++ src/app/Landing/page.tsx
@@ -156,9 +156,11 @@ const Landing = () => {
             <div className="grid grid-cols-2 gap-x-4 gap-y-6 sm:gap-y-8 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-12">
               <div className="flex flex-col items-center gap-2 sm:flex-row md:gap-4">
                 <div className="h-24 w-24 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-32 md:w-32">
-                  <img
+                  <Image
                     src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=256"
                     loading="lazy"
+                    width={256}
+                    height={256}
                     alt="Photo by Radu Florin"
                     className="h-full w-full object-cover object-center"
                   />
@@ -176,9 +178,11 @@ const Landing = () => {
 
               <div className="flex flex-col items-center gap-2 sm:flex-row md:gap-4">
                 <div className="h-24 w-24 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-32 md:w-32">
-                  <img
+                  <Image
                     src="https://images.unsplash.com/photo-1532073150508-0c1df022bdd1?auto=format&q=75&fit=crop&w=256"
                     loading="lazy"
+                    width={256}
+                    height={256}
                     alt="Photo by christian ferrer"
                     className="h-full w-full object-cover object-center"
                   />
@@ -196,9 +200,11 @@ const Landing = () => {
 
               <div className="flex flex-col items-center gap-2 sm:flex-row md:gap-4">
                 <div className="h-24 w-24 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-32 md:w-32">
-                  <img
+                  <Image
                     src="https://images.unsplash.com/photo-1463453091185-61582044d556?auto=format&q=75&fit=crop&w=256"
                     loading="lazy"
+                    width={156}
+                    height={156}
                     alt="Photo by Ayo Ogunseinde"
                     className="h-full w-full object-cover object-center"
                   />
